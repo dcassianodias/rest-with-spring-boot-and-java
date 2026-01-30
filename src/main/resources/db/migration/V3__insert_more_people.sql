69a761b8f2dd1378310f5c7a4391fcbec87273e6
Danilo Dias <dcassianodias@gmail.com>
2026-01-30 00:26:47 -0300

    refactor: scripts flyway

---
+++ src/main/resources/db/migration/V3__insert_more_people.sql
@@ -0,0 +1,4 @@
+INSERT INTO person (address, first_name, gender, last_name) VALUES
+    ('Paris - France', 'Marie', 'Female', 'Curie'),
+    ('Ulm - Germany', 'Albert', 'Male', 'Einstein'),
+    ('London - UK', 'Charles', 'Male', 'Darwin');
