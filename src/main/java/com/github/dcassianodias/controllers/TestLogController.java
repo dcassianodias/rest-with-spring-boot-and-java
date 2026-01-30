d87c3f1482b000b60a240d0c27174b363d79400d
Danilo Dias <dcassianodias@gmail.com>
2026-01-30 00:26:28 -0300

    refactor: route update for requests

--- src/main/java/com/github/dcassianodias/controllers/TestLogController.java
+++ src/main/java/com/github/dcassianodias/controllers/TestLogController.java
@@ -2,10 +2,12 @@ package com.github.dcassianodias.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequestMapping("/api/test/v1")
 public class TestLogController {
 
     @GetMapping("/log-test")
