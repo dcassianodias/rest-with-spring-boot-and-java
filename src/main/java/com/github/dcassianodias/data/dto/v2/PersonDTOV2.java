ec21e73ff4cc751e820f5c8d4d75c264a52ef00a
Danilo Dias <dcassianodias@gmail.com>
2026-01-30 00:24:47 -0300

    refactor: creating the DTO for V2

---
+++ src/main/java/com/github/dcassianodias/data/dto/v2/PersonDTOV2.java
@@ -0,0 +1,96 @@
+package com.github.dcassianodias.data.dto.v2;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+
+@JsonPropertyOrder({
+        "id",
+        "firstName",
+        "lastName",
+        "address",
+        "gender"
+})
+public class PersonDTOV2 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Date birthDate;
+
+    private String address;
+
+    private String gender;
+
+    public PersonDTOV2() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof PersonDTOV2 that)) return false;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getFirstName(), that.getFirstName()) &&
+                Objects.equals(getLastName(), that.getLastName()) && Objects.equals(getBirthDate(), that.getBirthDate())
+                && Objects.equals(getAddress(), that.getAddress()) && Objects.equals(getGender(), that.getGender());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getBirthDate(), getAddress(), getGender());
+    }
+}
