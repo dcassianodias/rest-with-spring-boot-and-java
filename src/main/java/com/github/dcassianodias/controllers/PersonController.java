c3fe9632510b163ceafcdd899c22cc660443f6d7
Danilo Dias <dcassianodias@gmail.com>
2026-02-05 23:20:28 -0300

    fix: remove unused imports.

--- src/main/java/com/github/dcassianodias/controllers/PersonController.java
+++ src/main/java/com/github/dcassianodias/controllers/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
