c716cd5f985dc1c25373a80c5ffc180f0a70de12
Danilo Dias <dcassianodias@gmail.com>
2026-02-05 23:17:28 -0300

    FEAT: serialization class.

---
+++ src/main/java/com/github/dcassianodias/serializer/GenderSerialize.java
@@ -0,0 +1,16 @@
+package com.github.dcassianodias.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+public class GenderSerialize extends JsonSerializer<String> {
+
+    @Override
+    public void serialize(String gender, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        String formattedGender = "Male".equals(gender) ? "M" : "F";
+        jsonGenerator.writeString(formattedGender);
+    }
+}
