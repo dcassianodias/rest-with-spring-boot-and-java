f416ee228cdd09187ab60b9fb7406030212d8769
Danilo Dias <dcassianodias@gmail.com>
2026-02-07 18:09:01 -0300

    feat: add produces for XML and YAML

--- src/main/java/com/github/dcassianodias/controllers/PersonController.java
+++ src/main/java/com/github/dcassianodias/controllers/PersonController.java
@@ -4,12 +4,12 @@ import com.github.dcassianodias.data.dto.v1.PersonDTO;
 import com.github.dcassianodias.data.dto.v2.PersonDTOV2;
 import com.github.dcassianodias.model.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -19,17 +19,26 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
     public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
-    @GetMapping
+    @GetMapping(produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
     public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
-    @PostMapping
+    @PostMapping(produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
     public ResponseEntity<PersonDTO> create(@RequestBody PersonDTO personDTO) {
         PersonDTO savedPerson = service.create(personDTO);
 
@@ -42,7 +51,10 @@ public class PersonController {
         return ResponseEntity.created(uri).body(savedPerson);
     }
 
-    @PostMapping("/v2")
+    @PostMapping(value = "/v2", produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
     public ResponseEntity<PersonDTOV2> createV2(@RequestBody PersonDTOV2 personDTO) {
         PersonDTOV2 savedPerson = service.createV2(personDTO);
 
@@ -55,12 +67,15 @@ public class PersonController {
         return ResponseEntity.created(uri).body(savedPerson);
     }
 
-    @PutMapping
+    @PutMapping(produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
     public PersonDTO update(@RequestBody PersonDTO personDTO) {
         return service.update(personDTO);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
