4e91eb82bb36daecab9892890d789e7a9036b5c2
Danilo Dias <dcassianodias@gmail.com>
2026-02-19 23:22:21 -0300

    feat: criação de annotations personalizadas para Responses

---
+++ src/main/java/com/github/dcassianodias/openapi/annotations/ApiResponsesCommonNotFound.java
@@ -0,0 +1,17 @@
+package com.github.dcassianodias.openapi.annotations;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponses({
+        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+})
+public @interface ApiResponsesCommonNotFound {}
